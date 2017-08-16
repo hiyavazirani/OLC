5355bb4c174ffcc19ac18766cff1fe175929905e
Hiya Vazirani <hiyavazirani@Hiyas-MacBook-Pro.local>
2017-08-16 17:04:08 -0400

    navbar fixed, alphabetical by CEO, other minor changes, quotes moved up

--- assets/js/navbar.js
+++
@@ -1,9 +0,0 @@
-$(".menu-btn").click(
-  function() {
-    $(".menu-btn").toggleClass("disBtn");
-    
-    $(".cross-btn").toggleClass("enCross");
-    
-    $('.menu-panel').toggleClass('showPanel');
-  }
-);
